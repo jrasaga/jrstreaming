c8385558b24ceb637acc6db3688826fdeda116bd
jrasaga <65923110+jrasaga@users.noreply.github.com>
2026-05-30 01:03:46 -0300

    Atualizar o correção de url download

--- src/app/api/update/route.ts
+++ src/app/api/update/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
   const updateInfo = {
     packageName: 'com.jr.streaming',
     versionCode: 25, // Aumente este número quando tiver uma nova versão
-    url: 'https://jrstreaming.vercel.app/downloads/app.apk' // URL do novo APK
+    url: 'https://drive.google.com/uc?export=download&id=1nztqX8D8mTT1N12P-Tx0quMucZNqu5W1' // URL do novo APK
   };
 
   return NextResponse.json(updateInfo);
