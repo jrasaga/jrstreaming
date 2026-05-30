ffe55a584cc54d598276d0571da9d309a3095a24
jrasaga <65923110+jrasaga@users.noreply.github.com>
2026-05-30 00:35:47 -0300

    Atualizar o update

--- src/app/api/update/route.ts
+++ src/app/api/update/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: NextRequest) {
 
   // Configuração da atualização
   const updateInfo = {
-    packageName: 'com.jr.streaming',',
+    packageName: 'com.jr.streaming',
     versionCode: 25, // Aumente este número quando tiver uma nova versão
     url: 'https://jrstreaming.vercel.app/downloads/app.apk' // URL do novo APK
   };
