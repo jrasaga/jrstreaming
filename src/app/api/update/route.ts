0b42a0869ead5962100bc3c72aaa2287f9c4cae7
jrasaga <65923110+jrasaga@users.noreply.github.com>
2026-05-30 01:16:39 -0300

    atualizando link download update

--- src/app/api/update/route.ts
+++ src/app/api/update/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
   const updateInfo = {
     packageName: 'com.jr.streaming',
     versionCode: 25, // Aumente este número quando tiver uma nova versão
-    url: 'https://drive.google.com/uc?export=download&id=1nztqX8D8mTT1N12P-Tx0quMucZNqu5W1' // URL do novo APK
+    url: 'https://www.dropbox.com/scl/fi/wkcybny2u95ydjr4kz1wq/jrstreaming.apk?rlkey=zxo1ck6dnk4m0fpc5hvna1gd4&dl=1' // URL do novo APK
   };
 
   return NextResponse.json(updateInfo);
